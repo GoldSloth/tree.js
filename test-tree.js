d9ec53efe41ba9a944f85ff546e85757e56d5c66
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2018-08-16 11:28:15 +0100

    Added logging file

--- test-tree.js
+++ test-tree.js
@@ -1,4 +1,18 @@
+var fs = require('fs')
+
+function logWrite(message) {
+    fs.writeFile("tree-log.log", message, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("Written to log");
+    }); 
+}
+
 var TC = require('./tree.js')
 
 var testTree3D = new TC.Tree3D()
 var testTree2D = new TC.Tree2D()
+
+logWrite('Stick stick stick')

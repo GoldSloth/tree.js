a0b92c635925993a36e78e1751a78d58c3c97cb6
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2018-08-16 12:13:22 +0100

    Planting de trees

--- tree.js
+++ tree.js
@@ -1,8 +1,26 @@
+const angle = 25
+const iterations = 3
+const axiom = ['X']
+const rules = {'X':'F+[[X]-X]-F[-FX]+X', 'F':'FF'}
+
+function applyRules(letter) {
+    for (var key in rules) {
+        var value = rules[key]
+        if (letter === key) {
+            return value
+        }
+    }
+}
+
 exports.Tree2D = function() {
     console.log('Tree2D created')
     this.type = 'Tree2D'
-    this.makeTree = function () {
-        return 'yay trees'
+    this.makeTree = function() {
+        var tree = axiom
+        for (i=0;i<iterations;i++) {
+            tree = tree.map(applyRules).map(x => x.split('')).flat()
+        }
+        return tree
     }
 }
 

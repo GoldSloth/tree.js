40175e233b93f90550451e109bd92548ff9f65a9
Ruth Waterfield <ruth@gareth.lan>
2018-08-18 22:12:24 +0100

    Allow post request and handle parameters

--- back-end/server.js
+++ back-end/server.js
@@ -1,23 +1,31 @@
-const TC = require('./tree.js')
-tree = new TC.Tree2D()
-tree.makeInstructions()
-tree.makeBranches()
-
 const http = require('http');
 const { Tree2D } = require('./tree');
 
 const hostname = '0.0.0.0';
 const port = 3001;
-const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/javascript')
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  var testTree2D = new Tree2D()
-testTree2D.makeInstructions()
-testTree2D.makeBranches()
-res.end(JSON.stringify(testTree2D.branches))
-  // res.end("window.branches = " + JSON.stringify(testTree2D.branches))
-});
+const server = http.createServer(function(request, response) {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'application/json')
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods',"POST")
+  response.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  response.setHeader('Access-Control-Max-Age', '86400')
+
+  if (request.method === 'POST') {
+    var body = ''
+    request.on('data', chunk => {
+      body += chunk.toString()
+    })
+    request.on('end', () => {
+      const treeConfig = JSON.parse(body)
+      var requestedBranches = new Tree2D(treeConfig.axiom, treeConfig.rules, treeConfig.iterations, treeConfig.angle, treeConfig.forwardMovement)
+      response.end(JSON.stringify(requestedBranches.makeTree()))
+    })
+  } else {
+    response.end()
+  }
+})
+
 server.listen(port, hostname, function() {
     console.log('Server running at http://'+ hostname + ':' + port + '/');
-});
+})

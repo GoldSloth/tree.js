6c08c316146a344a7d06717ec518b53570b5a87c
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2018-08-18 13:48:34 +0100

    Sending backend -> Frontend

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,17 @@
+const TC = require('./tree.js')
+tree = new TC.Tree2D()
+tree.makeInstructions()
+tree.makeBranches()
+
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3001;
 const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json');
+//  res.end('Hello World');
+    res.end(JSON.stringify(tree.branches))
 });
 server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
+    console.log('Server running at http://'+ hostname + ':' + port + '/');
 });

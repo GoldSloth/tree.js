6eeb09df0d3abb279eb6740112a0bc0d294967a5
Ruth Waterfield <ruth@gareth.lan>
2018-08-18 13:07:10 +0100

    server changes

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 const server = http.createServer(function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');

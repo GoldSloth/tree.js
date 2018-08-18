ae3ae6db2a93ae9b387f88e914f04fb73f50b16c
Ruth Waterfield <ruth@gareth.lan>
2018-08-18 14:40:13 +0100

    pre-dougify

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,15 @@
 const http = require('http');
+const { Tree2D } = require('./tree');
+
 const hostname = '0.0.0.0';
 const port = 3001;
 const server = http.createServer(function(req, res) {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.setHeader('Content-Type', 'text/javascript');
+  var testTree2D = new Tree2D()
+testTree2D.makeInstructions()
+testTree2D.makeBranches()
+  res.end("window.branches = " + JSON.stringify(testTree2D.branches))
 });
 server.listen(port, hostname, function() {
   console.log('Server running at http://'+ hostname + ':' + port + '/');
